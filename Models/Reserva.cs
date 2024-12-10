2617705ee9e90057c7c548a192c0faddd98c875b
Gleisson Neves Gazeta <81425442+gazetasi@users.noreply.github.com>
2024-12-10 13:39:25 -0300

    Add files via upload
    arquivos adicionados

---
+++ Models/Reserva.cs
@@ -0,0 +1,69 @@
+using System.Collections.Generic;
+
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        private int capacidade;
+
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+       
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+            Suite = new Suite();
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
+            
+            bool validaCapacidade = Suite.Capacidade >= hospedes.Count;
+            if (validaCapacidade)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
+                throw new System.InvalidOperationException("O número máximo de reservas já foi concluído!\n Atenção não existem mais vagas...");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
+            int quantidadeHospedes = this.Hospedes.Count;
+            return quantidadeHospedes;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            // TODO: Retorna o valor da diária
+            // Cálculo: DiasReservados X Suite.ValorDiaria
+
+
+            decimal valor = 0;
+            valor = DiasReservados * Suite.ValorDiaria;
+
+            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
+            bool validaDesconto = this.DiasReservados >= 10;
+            if (validaDesconto)
+            {
+                valor = valor * (10 / 100);
+            }
+
+            return valor;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DesafioProjetoHospedagem.Models;
+
+
+namespace DESAFIO_HOTELARIANET_DIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.WriteLine("######### CENÁRIO 1 TESTE ######################");
+
+            // Cria os modelos de hóspedes e cadastra na lista de hóspedes
+            List<Pessoa> hospedes = new List<Pessoa>();
+
+            Pessoa p1 = new Pessoa(nome: "Hóspede 1");
+            Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+
+            hospedes.Add(p1);
+            hospedes.Add(p2);
+            Pessoa p3 = new Pessoa(nome: "Hóspede 3");
+            Pessoa p4 = new Pessoa(nome: "Hóspede 4");
+            Pessoa p5 = new Pessoa(nome: "Hóspede 5");
+            Pessoa p6 = new Pessoa(nome: "Hóspede 6");
+            Pessoa p7 = new Pessoa(nome: "Hóspede 7");
+            Pessoa p8 = new Pessoa(nome: "Hóspede 8");
+            Pessoa p9 = new Pessoa(nome: "Hóspede 9");
+            Pessoa p10 = new Pessoa(nome: "Hóspede 10");
+
+            hospedes.Add(p3);
+            hospedes.Add(p4);
+            hospedes.Add(p5);
+            hospedes.Add(p6);
+          //  hospedes.Add(p7);
+           // hospedes.Add(p8);
+
+            // Cria a suíte
+            Suite suite = new Suite(tipoSuite: "Premium", capacidade: 10, valorDiaria: 50);
+
+            // Cria uma nova reserva, passando a suíte e os hóspedes
+            Reserva reserva = new Reserva(diasReservados: 8);
+            reserva.CadastrarSuite(suite);
+            reserva.CadastrarHospedes(hospedes);
+
+            // Exibe a quantidade de hóspedes e o valor da diária
+            Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
+            string resultado = String.Format("{0:F2}", reserva.CalcularValorDiaria());
+            Console.WriteLine($"Valor diária: {resultado}");
+            Console.ReadKey();
+           
+            /*
+                        Console.WriteLine("######### CENÁRIO 2 TESTE ######################");
+
+
+
+                        Pessoa p3 = new Pessoa(nome: "Hóspede 3");
+                        Pessoa p4 = new Pessoa(nome: "Hóspede 4");
+                        Pessoa p5 = new Pessoa(nome: "Hóspede 5");
+                        Pessoa p6 = new Pessoa(nome: "Hóspede 6");
+                        Pessoa p7 = new Pessoa(nome: "Hóspede 7");
+                        Pessoa p8 = new Pessoa(nome: "Hóspede 8");
+                        Pessoa p9 = new Pessoa(nome: "Hóspede 9");
+                        Pessoa p10 = new Pessoa(nome: "Hóspede 10");
+
+                        hospedes.Add(p3);
+                        hospedes.Add(p4);
+                        hospedes.Add(p5);
+                        hospedes.Add(p6);
+                        hospedes.Add(p7);
+                        hospedes.Add(p8);
+                        // hospedes.Add(p9);
+                        //  hospedes.Add(p10);
+
+
+                        reserva.Suite = new Suite(tipoSuite: "Simples", capacidade: 20, valorDiaria: 10);
+                        reserva.DiasReservados = 20;
+
+                        reserva.CadastrarSuite(reserva.Suite);
+                        reserva.CadastrarHospedes(hospedes);
+
+                        // Exibe a quantidade de hóspedes e o valor da diária
+                        Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
+                        Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+                        Console.ReadKey();
+                        */
+        }
+    }
+}
